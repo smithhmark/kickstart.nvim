ff13fb5a3c70dc2e648cc77d65fa61b6493a6c3f
Mark Smith <smith.h.mark@gmail.com>
2024-06-15 15:50:07 -0600

    making merges easier

--- init.lua
+++ init.lua
@@ -115,7 +115,7 @@ require('lazy').setup({
   },
 
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim',  opts = {} },
+  { 'folke/which-key.nvim', opts = {} },
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
@@ -352,7 +352,7 @@ local function live_grep_git_root()
   local git_root = find_git_root()
   if git_root then
     require('telescope.builtin').live_grep({
-      search_dirs = { git_root },
+      search_dirs = {git_root},
     })
   end
 end
