304e930c9af9e465e3dbb168aa856193b91d9ed6
Mark Smith <mark.smith@radicl.com>
2026-01-14 10:54:31 -0700

    feat: add `vim.opt.confirm = true` (#1384)

--- init.lua
+++ init.lua
@@ -156,6 +156,10 @@ vim.opt.scrolloff = 10
 
 -- setting a visual marker at 80-columns
 vim.opt.colorcolumn = '80'
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.opt.confirm = true
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
